0609e40a4102d48492c2cb5d9d023ef216212c70
Suyog Soti <ssoti2201@bvsd.org>
2014-12-08 18:52:06 -0700

    initail commit

---
+++ script.js
@@ -0,0 +1,10 @@
+//console.log("script connects!");
+$(document).ready(function(){
+  $('.unfinished').click(function(){
+    location.reload();
+    alert("Sorry, but this page has not been been finished yet");
+  });
+  $('#home_logo').click(function(){
+    location.reload();
+  });
+});
