8f9f65cf95f58d650b18347e92b6eb58bf1b66ba
Suyog Soti <ssoti2201@bvsd.org>
2014-12-14 15:08:22 -0700

    turn to pixels from percentages

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 //console.log("script connects!");
 $(document).ready(function(){
   $('.unfinished').click(function(){
-    location.reload();
+//    location.reload();
     alert("Sorry, but this page has not been been finished yet");
   });
   $('#home_logo').click(function(){
